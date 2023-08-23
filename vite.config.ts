7f786bbbf904b394f12d91b7e230b664dcfe8e95
william <william.pan@accuhit.net>
2023-08-23 21:20:22 +0800

    set pwa setting

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,13 @@ import { VitePWA } from 'vite-plugin-pwa'
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve: {},
-  plugins: [react(), VitePWA({ registerType: 'autoUpdate', injectRegister: 'inline' })],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+      },
+    }),
+  ],
 })
