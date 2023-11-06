b354937193dbe15f8fbe583670c71df007e74b3f
william <william.pan@accuhit.net>
2023-11-06 23:45:50 +0800

    feat:圖片壓縮並改成s3

--- src/components/PhotosModal.tsx
+++ src/components/PhotosModal.tsx
@@ -2,39 +2,9 @@ import { Div, Modal, Icon, Text } from 'atomize'
 import ImageViewer from 'react-simple-image-viewer'
 import { useState, useCallback } from 'react'
 const PhotosModal = ({ isOpen, onClose }: { isOpen: boolean; onClose: () => void }) => {
-  const images = [
-    'https://i.imgur.com/9T7EB35.jpg',
-    'https://i.imgur.com/TQwIPzJ.jpg',
-    'https://i.imgur.com/BNA3xvQ.jpg',
-    'https://i.imgur.com/EXkAFRf.jpg',
-    'https://i.imgur.com/T0Wa1Ev.jpg',
-    'https://i.imgur.com/Hj5YpN3.jpg',
-    'https://i.imgur.com/cVdGCGQ.jpg',
-    'https://i.imgur.com/ye8STyC.jpg',
-    'https://i.imgur.com/Tg25jER.jpg',
-    'https://i.imgur.com/gPAGnOq.jpg',
-    'https://i.imgur.com/hMsu5rA.jpg',
-    'https://i.imgur.com/QDpmUlY.jpg',
-    'https://i.imgur.com/coh6vVo.jpg',
-    'https://i.imgur.com/oU2p92q.jpg',
-    'https://i.imgur.com/hatBPqx.jpg',
-    'https://i.imgur.com/aZTCWFU.jpg',
-    'https://i.imgur.com/RbHiwNI.jpg',
-    'https://i.imgur.com/T6auYaL.jpg',
-    'https://i.imgur.com/vH6lTD6.jpg',
-    'https://i.imgur.com/2XvlYrr.jpg',
-    'https://i.imgur.com/FN7QYLw.jpg',
-    'https://i.imgur.com/sZbDv9p.jpg',
-    'https://i.imgur.com/tgd5Axt.jpg',
-    'https://i.imgur.com/Jmidf2V.jpg',
-    'https://i.imgur.com/eOIjSPb.jpg',
-    'https://i.imgur.com/KVafkZP.jpg',
-    'https://i.imgur.com/DkdU3d4.jpg',
-    'https://i.imgur.com/CSF98oF.jpg',
-    'https://i.imgur.com/N5b5zsD.jpg',
-    'https://i.imgur.com/DjRU4IE.jpg',
-    'https://i.imgur.com/bxXENnz.jpg',
-  ]
+  const images = new Array(56)
+    .fill(true)
+    .map((e, i) => `https://jiahao1123.s3.ap-northeast-2.amazonaws.com/weiju/${i + 1}.jpg`)
   const [currentImage, setCurrentImage] = useState(0)
   const [isViewerOpen, setIsViewerOpen] = useState(false)
   const openImageViewer = useCallback((index: number) => {
